a916b764931b90467b15b6c71bdabd59dbb8a0d9
Omkarmahanandia36 <83606909+Omkarmahanandia36@users.noreply.github.com>
2026-07-02 00:04:47 +0530

    Increase UploadThing maximum PDF size limit to 32MB

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -3,7 +3,7 @@ import { createUploadthing, type FileRouter } from "uploadthing/next";
 const f = createUploadthing();
 
 export const ourFileRouter = {
-  pdfUploader: f({ pdf: { maxFileSize: "4MB", maxFileCount: 1 } })
+  pdfUploader: f({ pdf: { maxFileSize: "32MB", maxFileCount: 1 } })
     .onUploadComplete(async ({ metadata, file }) => {
       console.log("Upload complete for userId:", metadata);
       console.log("file url", file.url);
